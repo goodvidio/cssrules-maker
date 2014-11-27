cc2544391489da189c3e0d661150b45d593e2a66
Adonis K <aklp08@gmail.com>
2014-11-27 11:04:56 +0200

    added app.js package.json

---
+++ app.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+var postcss = require('postcss');
+var fs = require('fs');
+
+var input = process.argv.slice(2)[0];
+var output = process.argv.slice(2)[1];
+
+if (input && output) {
+    var css = fs.readFileSync(input, 'utf8');
+    var result = '';
+
+    var contenter = postcss(function (css) {
+        css.eachRule(function (rule) {
+            result += "this.insertRule('" + rule.selector + "', '";
+
+            for (var i = 0; i < rule.childs.length; i++) {
+                result += rule.childs[i].prop + rule.childs[i].between + rule.childs[i].value + ';'
+            }
+
+            result += "');";
+        });
+    });
+
+    contenter.process(css).css;
+
+    result = result.replace(/(\r\n|\n|\r)/gm,"");
+
+    var css = fs.writeFileSync(output, result, 'utf8');
+} else {
+    throw Error('Illegal amount of parameters, input and output paths as parameters are required (ie: myapp style.css result.js)');
+}
