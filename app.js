040ff49f4c38509b30ab9caf4dde504783687776
Adonis K <aklp08@gmail.com>
2014-12-10 13:07:57 +0200

    refactor the code to parse the rules in correct order, also made it easier to understand and extend

--- app.js
+++ app.js
@@ -4,60 +4,90 @@ var postcss = require('postcss');
 var fs = require('fs');
 var _ = require('underscore');
 
-var input = process.argv.slice(2)[0];
-var output = process.argv.slice(2)[1];
+var inputFile = process.argv.slice(2)[0];
+var outputFile = process.argv.slice(2)[1];
 
-if (input && output) {
-    var css = fs.readFileSync(input, 'utf8');
-    var result;
+if (inputFile && outputFile) {
+    var css = fs.readFileSync(inputFile, 'utf8');
+    var output = '';
 
-    var contenter = postcss(function (css) {
-        result = 'define(function () { return function (module) {';
+    var parseRule = function (rule) {
+        var result;
 
-        css.eachRule(function (rule) {
-            if (rule.parent.type === 'root') {
-                result += "module.insertRule('" + rule.selector + "', '";
+        result = rule.selector + "', '";
+
+        for (var i = 0; i < rule.childs.length; i++) {
+            result += rule.childs[i].prop + rule.childs[i].between + rule.childs[i].value + ';';
+        }
+
+        result = "module.insertRule('" + result + "');";
+
+        return result;
+    };
+
+    var parseAtRule = function (rule) {
+        var result;
+
+        if (rule.name === 'import') {
+            result = parseImportAtRule(rule);
+        } else if (rule.name === 'media') {
+            result = parseMediaAtRule(rule);
+        } else{
+            throw new Error('Unsupported atRule');
+        }
+
+        return result;
+    };
+
+    parseImportAtRule = function (rule) {
+        var result;
+
+        result = "module.injectRule('@" + rule.name + ' ' + rule.params + ";');";
+
+        return result;
+    };
+
+    parseMediaAtRule = function (rule) {
+        var result = '';
+
+        if (rule.childs) {
+            var query = "@" + rule.name + " " + rule.params + "{";
+
+            _.each(rule.childs, function (rule) {
+                result = query + rule.selector + "{";
 
                 for (var i = 0; i < rule.childs.length; i++) {
                     result += rule.childs[i].prop + rule.childs[i].between + rule.childs[i].value + ';';
                 }
 
-                result += "');";
+                result += '}}';
+
+                result = "module.injectRule('" + result + "');";
+            });
+        }
+
+        return result;
+    };
+
+    var contenter = postcss(function (css) {
+        output = 'define(function () { return function (module) {';
+
+        css.each(function (rule, index) {
+            if (rule.type === 'rule') {
+                output += parseRule(rule);
+            } else if (rule.type === 'atrule') {
+                output += parseAtRule(rule);
             }
-        });
+        })
 
-        css.eachAtRule(function (atrule) {
-            if (atrule.name === 'import') {
-                result += "module.injectRule('@" + atrule.name + ' ' + atrule.params + ";');";
-            } else if (atrule.name === 'media') {
-                if (atrule.childs) {
-
-                    _.each(atrule.childs, function (rule) {
-                        var temp = "@" + atrule.name + " " + atrule.params + "{" + rule.selector + "{";
-
-                        for (var i = 0; i < rule.childs.length; i++) {
-                            temp += rule.childs[i].prop + rule.childs[i].between + rule.childs[i].value + ';';
-                        }
-
-                        temp += '}}';
-
-                        result += "module.injectRule('" + temp + "');";
-                    });
-                }
-            } else{
-                throw new Error('Unsupported atRule');
-            }
-        });
-
-       result += '}});';
+        output += '}});';
     });
 
     contenter.process(css).css;
 
+    output = output.replace(/(\r\n|\n|\r)/gm,"");
 
-    result = result.replace(/(\r\n|\n|\r)/gm,"");
-
-    var css = fs.writeFileSync(output, result, 'utf8');
+    fs.writeFileSync(outputFile, output, 'utf8');
 } else {
-    throw Error('Illegal amount of parameters, input and output paths as parameters are required (ie: myapp style.css result.js)');
+    throw new Error('Illegal amount of parameters, input and output paths as parameters are required (ie: myapp style.css result.js)');
 }
