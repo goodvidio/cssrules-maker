0e408c50eed24d94390a0e1b1be15cd63fababec
Adonis K <aklp08@gmail.com>
2017-04-28 11:06:55 +0300

    add test case for font-face declarations with !important, switch from npm run test-watch to npm run test:watch

--- test/fixtures/fontFaces.js
+++ test/fixtures/fontFaces.js
@@ -5,7 +5,19 @@ const cssrules = require(path.resolve('./src/app'));
 
 test('font-face declaration', t => {
     const actual = `@font-face {
-      font-family: "Open Sans";
+      font-family: "Open Sans" !important;
+      font-style: normal;
+      font-weight: 400;
+      src: local("Open Sans"), local("OpenSans"), url("data:application/x-font-woff;base64,aaa=") format("woff"); }`;
+    const expected = ['@font-face{font-family: "Open Sans";font-style: normal;font-weight: 400;src: local("Open Sans"), local("OpenSans"), url("data:application/x-font-woff;base64,aaa=") format("woff");}'];
+
+    return cssrules(actual)
+        .then(result => t.deepEqual(result, expected));
+});
+
+test('font-face declaration with !important', t => {
+    const actual = `@font-face {
+      font-family: "Open Sans" !important;
       font-style: normal;
       font-weight: 400;
       src: local("Open Sans"), local("OpenSans"), url("data:application/x-font-woff;base64,aaa=") format("woff"); }`;
